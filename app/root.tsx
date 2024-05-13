d966a76fbf294d1a93c7cc1f013b34775b1f86ff
masa-e <makkin006@gmail.com>
2024-05-13 15:52:15 +0900

    add: remix introduction

--- app/root.tsx
+++ app/root.tsx
@@ -2,11 +2,30 @@ import {
   Form,
   Links,
   Meta,
+  Outlet,
   Scripts,
   ScrollRestoration,
+  Link,
+  useLoaderData,
 } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import appStylesHref from "./app.css";
+
+import { getContacts } from "./data";
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: appStylesHref },
+];
+
+export const loader = async () => {
+  const contacts = await getContacts();
+  return json({ contacts });
+};
 
 export default function App() {
+  const { contacts } = useLoaderData<typeof loader>();
+
   return (
     <html lang="en">
       <head>
@@ -36,14 +55,41 @@ export default function App() {
           <nav>
             <ul>
               <li>
-                <a href={`/contacts/1`}>Your Name</a>
+                <Link to={`/contacts/1`}>Your Name</Link>
               </li>
               <li>
-                <a href={`/contacts/2`}>Your Friend</a>
+                <Link to={`/contacts/2`}>Your Friend</Link>
               </li>
             </ul>
+            {contacts.length ? (
+             <ul>
+                {contacts.map((contact) => (
+                 <li key={contact.id}>
+                   <Link to={`contacts/${contact.id}`}>
+                      {contact.first || contact.last ? (
+                        <>
+                          {contact.first} {contact.last}
+                        </>
+                      ) : (
+                        <i>No Name</i>
+                      )}{" "}
+                      {contact.favorite ? (
+                        <span>★</span>
+                      ) : null}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            ) : (
+              <p>
+                <i>No contacts</i>
+             </p>
+            )}
           </nav>
         </div>
+          <div id="detail">
+            <Outlet />
+          </div>
 
         <ScrollRestoration />
         <Scripts />
---
+++ app/routes/carstay.tsx
@@ -0,0 +1,7 @@
+export default function Carstay() {
+  return (
+    <div>
+      <h1>Hello World!!</h1>
+    </div>
+  );
+}
